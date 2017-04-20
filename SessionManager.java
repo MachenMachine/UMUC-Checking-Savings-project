278a05cb0ef28357cafa7e81e2d8e1c128127958
cmaginnis <conor.maginnis@hobsons.com>
2017-04-20 12:00:01 -0400

    Add files via upload

--- SessionManager.java
+++ SessionManager.java
@@ -12,6 +12,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 
 public class SessionManager {
@@ -21,6 +22,7 @@ public class SessionManager {
     Statement statement;
     ResultSet resultSet;
     String hashPassword;
+	//private String recievingaccountType;
     
     // Constructor
     SessionManager(){
@@ -64,18 +66,38 @@ public class SessionManager {
     }
     
     public String getTransactionHistory(String loginName, String accountType) throws SQLException{
-        String history;
+        String history ="";
         statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(
-                "select balanceChange from transactionHistory WHERE username = '" + loginName +"' and accountType = '" + accountType +"';");
-        resultSet.next(); 
-        double balanceInt = resultSet.getDouble(1);
-        history = "$" + balanceInt;
+                "select * from transactionHistory WHERE username = '" + loginName +"' and accountType = '" + accountType +"' ORDER BY transactionDate;");
+        
+        while(resultSet.next()){ //if empty, returns false. Will not get an error as it will never be a null row
+        	history += "Balance change of $" + resultSet.getDouble("balanceChange") + " on date " + resultSet.getDate("transactionDate") +"\n";
+        }
+        
+        
         return history; 
     }
     
-    public Boolean transferMoney(String loginName, String transferringaccountType, String receivingAccountType, String transferAmount) throws SQLException{
+    public ResultSet getTransactionHistoryForInterestCalculator(String loginName, String accountType) throws SQLException{
+        
+        statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(
+                "select * from transactionHistory WHERE username = '" + loginName +"' and accountType = '" + accountType +"' ORDER BY transactionDate;");
+        
+        return resultSet;
+    }
+    
+    public Boolean transferMoney(String loginName, String transferringAccountType, String receivingAccountType, String transferAmount) throws SQLException{
         Boolean transferStatus;
+        Date date= new Date();
+        String todayDate = date.getYear()+1900+"-"+Integer.toString(date.getMonth()+1)+"-"+date.getDate();
+        double transferAmountDouble = Double.parseDouble(transferAmount);
+        statement = connection.createStatement();
+        statement.execute("UPDATE accountInformation SET accountBalance = '" + transferAmountDouble+"', lastUpdate = '" + todayDate +"' WHERE username = '"+ loginName+"' AND accountType = '"+ receivingAccountType+ "';");
+        statement.execute("UPDATE accountInformation SET accountBalance = '" + -transferAmountDouble+"', lastUpdate = '" + todayDate +"' WHERE username = '"+ loginName+"' AND accountType = '"+ transferringAccountType+ "';");
+        statement.execute("INSERT INTO transactionHistory values ('"+loginName+"', '"+ receivingAccountType+"', "+ transferAmountDouble +", '"+todayDate+"');");
+        statement.execute("INSERT INTO transactionHistory values ('"+loginName+"', '"+ transferringAccountType+"', "+ -transferAmountDouble +", '"+todayDate+"');");
         transferStatus = true;
         return transferStatus; 
     }
