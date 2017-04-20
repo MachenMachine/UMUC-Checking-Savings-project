704764937775d8ee0e97e53a5be8f543d1a635d7
MatthewPNielsen <MatthewPNielsen@users.noreply.github.com>
2017-04-19 19:29:22 -0700

    SessionManager.java
    Rough version of the class that can be used to test the GUI. It needs function validation for 2 methods and construction of 2 non-functional methods. Please see group discussion in UMUC for details.

--- SessionManager.java
+++ SessionManager.java
@@ -1,3 +1,12 @@
+/*  File:       SessionManager.java
+ *  Authors:    Matthew Nielsen, Conor Maginnis
+ *  Date:       4-18-2017
+ *  Purpose:    To provide the connection to the sql database and facilitate the
+ *              interface between it and the application.
+ */
+
+
+// Imports
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -6,34 +15,81 @@ import java.sql.Statement;
 
 
 public class SessionManager {
-	Connection connection = null; //keeps an open session until logout.
-	SessionManager(){
-		connectToDatabase(); //allows the database to only maintain one connection as it can only be reached from the constructor
-	}
+    
+    // Fields
+    Connection connection = null;
+    Statement statement;
+    ResultSet resultSet;
+    String hashPassword;
+    
+    // Constructor
+    SessionManager(){
+        connectToDatabase();
+    }
 
-	private void connectToDatabase() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver"); //do not change. If you get an error, download the driver from https://www.javatpoint.com/src/jdbc/mysql-connector.jar and add it to the build path
-		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}  
-		try {
-			connection = DriverManager.getConnection("jdbc:mysql://group3db.ce5lidsgsgti.us-east-1.rds.amazonaws.com:3306/group3db","group3","group3password"); //this is static, do not change this or none of the DB connectivity will work
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  		
-}
-		public String getBalance(String username, String accountType) throws SQLException{
-			String balance;
-			Statement statement = connection.createStatement(); //allows the connection to create a new statement
-			ResultSet resultSet = statement.executeQuery(
-					"select accountBalance from accountInformation WHERE username = '" + username +"' and accountType = '" + accountType +"';"); //passes the query into the statement for execution. the ' are needed for STRING datatypes
-			resultSet.next(); //puts it onto row 1
-			double balanceInt = resultSet.getDouble(1); //gets the double from the first column in the first row
-			balance = "$" + balanceInt; //formats
-			return balance; 
-		}
+    // Methods
+    private void connectToDatabase() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); //do not change. If you get an error, download the driver from https://www.javatpoint.com/src/jdbc/mysql-connector.jar and add it to the build path
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }  
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://group3db.ce5lidsgsgti.us-east-1.rds.amazonaws.com:3306/group3db","group3","group3password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }  		
+    }
+    
+    public Boolean verifyLogin(String loginName, String loginPassword) {
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(
+                "select username from loginInformation WHERE username = '" + loginName +"' and password = '" + loginPassword +"';");
+            return true;
+        } catch (SQLException ex1){
+            return false;
+        }
+    }
+    
+    public String getBalance(String loginName, String accountType) throws SQLException{
+        String balance;
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery(
+                "select accountBalance from accountInformation WHERE username = '" + loginName +"' and accountType = '" + accountType +"';");
+        resultSet.next();
+        double balanceInt = resultSet.getDouble(1);
+        balance = "$" + balanceInt;
+        return balance; 
+    }
+    
+    public String getTransactionHistory(String loginName, String accountType) throws SQLException{
+        String history;
+        statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(
+                "select balanceChange from transactionHistory WHERE username = '" + loginName +"' and accountType = '" + accountType +"';");
+        resultSet.next(); 
+        double balanceInt = resultSet.getDouble(1);
+        history = "$" + balanceInt;
+        return history; 
+    }
+    
+    public Boolean transferMoney(String loginName, String transferringaccountType, String receivingAccountType, String transferAmount) throws SQLException{
+        Boolean transferStatus;
+        transferStatus = true;
+        return transferStatus; 
+    }
+    
+    public String calculateInterest(String loginName, String accountType) throws SQLException{
+        String interest = "0";
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery(
+                "select accountBalance from accountInformation WHERE username = '" + loginName +"' and accountType = '" + accountType +"';");
+        resultSet.next();
+        double balanceInt = resultSet.getDouble(1);
+        if (accountType == "checking") interest = "$" + (balanceInt*0.0004);
+        if (accountType == "savings") interest = "$" + (balanceInt*0.0006);
+        return interest; 
+    }
 
-}
+}// End of class SessionManager
